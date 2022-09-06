aca83b9f1a8a5e195442a73f29b939ae91908b25
Beiusanu Horia <horibeiu@gmail.com>
2022-09-06 21:42:04 +0300

    fixed url path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://crypto-hub-api.herokuapp.com/'
+const baseURL = 'https://crypto-hub-api.herokuapp.com'
 
 const userInput = document.querySelector('.login-username')
 const passInput = document.querySelector('.login-password')
@@ -8,7 +8,7 @@ btn.addEventListener('click', async () => {
     const email = userInput.value
     const password = passInput.value
     try {
-        const { data } = await axios.post(`${baseURL}/api/v1/auth/login`, { email, password })
+        const { data } = await axios.post(`${baseURL}api/v1/auth/login`, { email, password })
         console.log(data);
         localStorage.setItem('token', data.token)
         location.href = './mainMenu/mainMenu.html'
